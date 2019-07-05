3ba2557d7ddaa6375aa18267457b2253e9ff1c83
JJ <jj.orozcolopez.13@gmail.com>
2019-07-05 10:59:19 -0600

    VAL vs VAR and concat strings
    Understanding the difference between VAL and VAR variables in Kotlin.
    Using concatenation of strings and variables' values.

--- src/MainClass.kt
+++ src/MainClass.kt
@@ -2,5 +2,8 @@
 
 fun main (args: Array<String>)
 {
-    println("Hi JJ!")
+    val realName = "Juan José" // Se usa VAL para variables inmutables (constantes)
+    var alias = "JJ" // Se usa VAR para variables mutables
+
+    println("Hi $alias!! ($realName)") // Concatenación con operador $
 }
